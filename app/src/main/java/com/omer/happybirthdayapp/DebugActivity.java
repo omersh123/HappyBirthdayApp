87aa0f0297675f01fdfc6b0210e78cea7d1ff6bc
OmerSh <o.shalev1@gmail.com>
2018-05-13 00:41:30 +0300

    task 1

---
+++ app/src/main/java/com/omer/happybirthdayapp/DebugActivity.java
@@ -0,0 +1,14 @@
+package com.omer.happybirthdayapp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class DebugActivity extends AppCompatActivity {
+
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_debug);
+    }
+}
