c57f4f7c8039df7fedc0d2b4527fe5bc2a9f717e
omersh123 <o.shalev1@gmail.com>
2018-05-15 07:11:09 +0300

    task 5

--- app/src/main/java/com/omer/happybirthdayapp/activities/BirthdayActivity.java
+++ app/src/main/java/com/omer/happybirthdayapp/activities/BirthdayActivity.java
@@ -4,7 +4,9 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.AppCompatButton;
 import android.support.v7.widget.AppCompatImageButton;
@@ -19,6 +21,9 @@ import com.omer.happybirthdayapp.utils.AddImageUtil;
 
 import org.json.JSONException;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Date;
 import java.util.Random;
 
@@ -92,6 +97,17 @@ public class BirthdayActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 finish();
+            }
+        });
+        shareBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                closeBtn.setVisibility(View.INVISIBLE);
+                shareBtn.setVisibility(View.INVISIBLE);
+                Bitmap bitmap = getScreenShot(getWindow().getDecorView().findViewById(android.R.id.content));
+                shareImage(bitmap);
+                closeBtn.setVisibility(View.VISIBLE);
+                shareBtn.setVisibility(View.VISIBLE);
             }
         });
     }
@@ -194,12 +210,38 @@ public class BirthdayActivity extends AppCompatActivity {
         }
     }
 
-    public void saveData() {
+    public static Bitmap getScreenShot(View view) {
+        View screenView = view.getRootView();
+        screenView.setDrawingCacheEnabled(true);
+        Bitmap bitmap = Bitmap.createBitmap(screenView.getDrawingCache());
+        screenView.setDrawingCacheEnabled(false);
+        return bitmap;
+    }
+
+    private void shareImage(Bitmap bitmap){
+        //taken from http://androidtechpoint.blogspot.co.il/2017/09/android-sharing-image-using-share-intent-without-saving-to-external-memory.html
         try {
-            baby.save();
-        } catch (JSONException e) {
+            File cachePath = new File(this.getCacheDir(), "images");
+            cachePath.mkdirs();
+            FileOutputStream stream = new FileOutputStream(cachePath + "/image.png");
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+            stream.close();
+
+        } catch (IOException e) {
             e.printStackTrace();
-            Toast.makeText(this, "Error while saving model to remote disk", Toast.LENGTH_SHORT).show();
+        }
+        File imagePath = new File(this.getCacheDir(), "images");
+        File newFile = new File(imagePath, "image.png");
+        Uri contentUri = FileProvider.getUriForFile(this, this.getPackageName() + ".fileprovider", newFile);
+
+        if (contentUri != null) {
+            Intent shareIntent = new Intent();
+            shareIntent.setAction(Intent.ACTION_SEND);
+            shareIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            shareIntent.setDataAndType(contentUri, getContentResolver().getType(contentUri));
+            shareIntent.putExtra(Intent.EXTRA_STREAM, contentUri);
+            shareIntent.setType("image/png");
+            startActivity(Intent.createChooser(shareIntent, "Choose an app"));
         }
     }
 
